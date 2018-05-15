d2ca714e657eedb0047caa485bf8b934b5ffc907
Gustavo Ordaz <ordazsgustavo@gmail.com>
2018-05-14 23:10:23 -0500

    Improvements
    shared functions and lazy loading

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,43 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-class OrderSummary extends Component {
-  // This could be a functional component,
-  // doesn't have to be a class component
-  componentWillUpdate () {
-    console.log('[OrderSummary] WillUpdate');
-  }
+const OrderSummary = props => {
+  const ingredientSummary = Object.keys(props.ingredients)
+    .map(ingKey => {
+      return (
+        <li key={ingKey}>
+          <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {props.ingredients[ingKey]}
+        </li>
+      );
+    });
 
-  render () {
-    const ingredientSummary = Object.keys(this.props.ingredients)
-      .map(ingKey => {
-        return (
-          <li key={ingKey}>
-            <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {this.props.ingredients[ingKey]}
-          </li>
-        );
-      });
-
-    return (
-      <Aux>
-        <h3>Order Summary</h3>
-        <p>A deliciuos burger with the following ingredients:</p>
-        <ul>
-          {ingredientSummary}
-        </ul>
-        <p>Total Price: <strong>{this.props.price.toFixed(2)}</strong></p>
-        <p>Continue to Checkout?</p>
-        <Button
-          btnType="Danger"
-          clicked={this.props.purchaseCancelled}>CANCEL</Button>
-        <Button
-          btnType="Success"
-          clicked={this.props.purchaseContinued}>CONTINUE</Button>
-      </Aux>
-    );
-  }
+  return (
+    <Aux>
+      <h3>Order Summary</h3>
+      <p>A deliciuos burger with the following ingredients:</p>
+      <ul>
+        {ingredientSummary}
+      </ul>
+      <p>Total Price: <strong>{props.price.toFixed(2)}</strong></p>
+      <p>Continue to Checkout?</p>
+      <Button
+        btnType="Danger"
+        clicked={props.purchaseCancelled}>CANCEL</Button>
+      <Button
+        btnType="Success"
+        clicked={props.purchaseContinued}>CONTINUE</Button>
+    </Aux>
+  );
 }
 
 export default OrderSummary;
